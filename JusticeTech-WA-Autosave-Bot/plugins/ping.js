29e6601e55f9fa4f0a68b0ad62ea8f86ddd06462
JusticeTechAutosave <justicetechautosave@gmail.com>
2026-02-19 09:47:32 +0100

    Delete JusticeTech-WA-Autosave-Bot directory

--- JusticeTech-WA-Autosave-Bot/plugins/ping.js
+++
@@ -1,8 +0,0 @@
-// plugins/ping.js
-module.exports = {
-  name: "Ping",
-  category: "tools",
-  desc: "Quick sanity check",
-  command: ["ping"],
-  run: async ({ reply }) => reply("pong"),
-};
--- JusticeTech-WA-Autosave-Bot/settings/bank.js
+++
@@ -1,6 +0,0 @@
-// settings/bank.js
-module.exports = {
-  bankName: "Opay",
-  accountNumber: "8159050063",
-  accountName: "Justice",
-};
